e19ecaa3676116f6c6516647d9d18d7a60610210
Akash Shivakumar <53287284+Akashshi@users.noreply.github.com>
2025-06-01 16:16:53 +0530

    Update usp_POST_Notice.sql
    Added validity field column as part of Input JSON in order to place the validity for the particular notice

--- Database Scripts/Stored Procedures/Notice Board Management/usp_POST_Notice.sql
+++ Database Scripts/Stored Procedures/Notice Board Management/usp_POST_Notice.sql
@@ -6,16 +6,18 @@ BEGIN
 
     DECLARE @title VARCHAR(100),
             @message TEXT,
-            @posted_by VARCHAR(100);
+            @posted_by VARCHAR(100),
+			@Validity DATE;
 
     -- Parse JSON input
     SET @title      = JSON_VALUE(@jsonInput, '$.title');
     SET @message    = JSON_VALUE(@jsonInput, '$.message');
     SET @posted_by  = JSON_VALUE(@jsonInput, '$.posted_by');
+	SET @Validity	= TRY_CAST(JSON_VALUE(@jsonInput, '$.Validity') AS DATE);
 
     BEGIN TRY
-        INSERT INTO TBL_DIM_Notices (title, message, posted_by)
-        VALUES (@title, @message, @posted_by);
+        INSERT INTO TBL_DIM_Notices (title, message, posted_by,validity)
+        VALUES (@title, @message, @posted_by,@validity);
 
         PRINT 'Notice posted successfully.';
     END TRY
